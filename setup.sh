cdbd614edd732b6ab9216543a2f48c4dbc012169
Ethan Liu <ethantsliu@gmail.com>
2023-02-12 05:11:55 +0000

    deleted pip install flax

--- setup.sh
+++ setup.sh
@@ -3,13 +3,12 @@ sudo mv ./ngrok /usr/bin/ngrok
 ngrok config add-authtoken 2L9sr4R8oew4IbODsM4ookVF7Bk_43Kd4JSQnVWvKX8RmoYpS
 
 pip install --upgrade pip 
-pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+#pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 pip install --upgrade git+https://github.com/google/flax.git
-pip install Flax
 pip install flask
 
-pip install --upgrade jaxlib
+#pip install --upgrade jaxlib
 pip install einops
 
 ngrok http 91
