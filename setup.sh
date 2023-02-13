3ef62e7ead4ecdab1d6bfcb3534f5b1efa9bc419
Ethan <ethantsliu@gmail.com>
2023-02-13 02:15:48 +0000

    end-to-end worked

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ pip install --upgrade git+https://github.com/google/flax.git
 pip install flask
 
 pip install --upgrade jaxlib
-pip install einops
+pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
+sudo apt update && sudo apt install ffmpeg pip setuptools-rust einops
 
 ngrok http 91
